488f03cd9545a09d30b3712628fee30ba87d28b9
John Wilson <johnwilson000007@gmail.com>
2018-11-06 20:57:44 -0800

    day 1 classwork

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,23 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-intro-p1';
+  title: String = 'Some Angular Garbage';
+  name: String = 'John';
+  currentDay: any = new Date().getDay();
+  days: Object = {
+    0: 'Sunday',
+    1: 'Monday',
+    2: 'Tuesday',
+    3: 'Wednesday',
+    4: 'Thursday',
+    5: 'Friday',
+    6: 'Saturday'
+  };
+
+  renderThisDay() {
+    return this.days[this.currentDay];
+  }
 }
