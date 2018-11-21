26895d380746c82ee69794fd9be7637caf5611c0
John Wilson <johnwilson000007@gmail.com>
2018-11-20 20:58:52 -0800

    made an address book that sorts in alphabetical order

--- src/app/mocks/mock-todos.ts
+++ src/app/mocks/mock-todos.ts
@@ -1,9 +1,11 @@
 import { Todo } from '../models/todo';
 
+import { TodoInterface } from '../interfaces/todo-interface';
+
 export const TODOS: Todo[] = [
-    { id: 1, name: 'Take out trash', tags: [], isCompleted: false, isDeleted: false },
-    { id: 2, name: 'Become the trash', tags: [], isCompleted: false, isDeleted: false },
-    { id: 3, name: 'Eat the food', tags: [], isCompleted: false, isDeleted: false },
-    { id: 4, name: 'Befriend puppets', tags: [], isCompleted: false, isDeleted: false },
-    { id: 5, name: 'Join Sesame Street', tags: [], isCompleted: false, isDeleted: false }
+    { id: 1, name: 'Take out trash', tags: ['Weekly', 'Monthly'], isCompleted: false, isDeleted: false },
+    { id: 2, name: 'Become the trash', tags: ['Daily', 'Bi-Weekly'], isCompleted: false, isDeleted: false },
+    { id: 3, name: 'Eat the food', tags: ['Weekly', 'Daily'], isCompleted: false, isDeleted: false },
+    { id: 4, name: 'Befriend puppets', tags: ['Daily', 'Hourly'], isCompleted: false, isDeleted: false },
+    { id: 5, name: 'Join Sesame Street', tags: ['Daily', 'Monthly', 'Yearly'], isCompleted: false, isDeleted: false }
 ];
---
+++ src/app/models/contact.ts
@@ -0,0 +1,10 @@
+export class Contact {
+  id: number;
+  name: string;
+  email: string;
+  phone: string;
+  tags: string[];
+  isDeleted = false;
+}
+
+
