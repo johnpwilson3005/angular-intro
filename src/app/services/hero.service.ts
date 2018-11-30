6b20c2d5f9f196a644ce3054bf7cadbe05faeff5
John Wilson <johnwilson000007@gmail.com>
2018-11-29 20:49:31 -0800

    added more sorting functionality to heroes by super powers

--- src/app/services/hero.service.ts
+++ src/app/services/hero.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 
 import { HEROES } from '../mocks/mock-heroes';
 import { HeroInterface } from '../interfaces/hero-interface';
+import { HeroesComponent } from '../heroes/heroes.component';
 
 @Injectable({
   providedIn: 'root'
@@ -12,13 +13,19 @@ export class HeroService {
   constructor() { }
 
   getHeroes(filter: string): HeroInterface[] {
-    if (filter === 'all') { return HEROES; }
+    if (!filter) { return HEROES; }
       return HEROES.filter( hero => {
         if (filter === 'hero') {
           return hero.hero;
         } else if (filter === 'villian' && !hero.hero) {
           return hero;
         }
+
+        return hero.powers.includes(filter);
       });
     }
+
+  getIndividualHero(params: object): HeroInterface {
+    return HEROES.find(hero => hero.id === +params['id']);
   }
+}
